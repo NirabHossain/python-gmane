4e976edf260e1074d770894b7dd52f29af560e7c
NirabHossain <nirabamth@gmail.com>
2021-05-23 02:56:54 +0600

    previous

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','indiana.edu','berkeley.edu','ucdavis.edu','cam.ac.uk','mac.com','gmail.com','uct.ac.za','yale.edu','ufp.pt'],
+['2005',57,12,12,11,7,12,10,14,5,10],
+['2006',323,110,99,93,95,86,79,65,63,57]
+];
